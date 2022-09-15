442e22b1a28b04e857372b69ac9e7204b791792e
tracer-ics2022 <tracer@njuics.org>
2022-09-15 14:42:46 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:42:46 up 1 day, 23:06,  1 user,  load average: 0.35, 0.42, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -66,7 +66,6 @@ static int cmd_si(char *args){
 static int cmd_info(char *args){
   char *arg = strtok(NULL, " ");
   Log("%s", arg);
-  printf("%s\n", arg);
   if(arg == NULL) printf("info: Too few arguments \n");
   else if(strcmp(arg, "r")); //! TODO
   else if(strcmp(arg, "w")); //! TODO
