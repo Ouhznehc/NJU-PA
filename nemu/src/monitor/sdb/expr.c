a0861c9ac9c691c57271b914d7a67d63710ae8be
tracer-ics2022 <tracer@njuics.org>
2022-09-18 21:38:04 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:38:04 up 5 days,  6:02,  2 users,  load average: 2.99, 2.63, 2.61

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -191,6 +191,7 @@ int eval(int p, int q, int *success) {
     if(tokens[p].type == TK_NUM_10) ans = atoi(tokens[p].str);
     else if(tokens[p].type == TK_NUM_16) sscanf(tokens[p].str, "%x", &ans);
     else ans = isa_reg_str2val(tokens[p].str + 1, success);
+    printf("%d\n", ans);
     if(*success) return ans;
     else return 0;
   }
