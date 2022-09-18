f487d996a27f16dcdc94bc6120457d04a85e7fb4
tracer-ics2022 <tracer@njuics.org>
2022-09-18 22:14:17 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:14:17 up 5 days,  6:38,  2 users,  load average: 2.86, 2.60, 2.52

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -36,6 +36,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, int *success) {
   Log("%s", s);
+  if(strcmp(s, "0") == 0) return cpu.gpr[0];
   for(int i = 0; i < 32; i++)
     if(strcmp(s, regs[i]) == 0)
       return cpu.gpr[i];
