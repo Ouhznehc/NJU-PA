37f6d7ae50e22168e7e030e58f537d144d66e0b2
tracer-ics2022 <tracer@njuics.org>
2022-10-02 12:37:11 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:37:11 up 14:34,  2 users,  load average: 0.84, 0.88, 0.82

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_B,
   TYPE_N, // none
 };
 
@@ -32,6 +32,17 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+//! begin my code
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) |\
+                           (SEXT(BITS(i, 30, 21), 10) << 1) |\
+                           (SEXT(BITS(i, 20, 20), 1) << 11) |\
+                           (SEXT(BITS(i, 19, 12), 8) << 12) ; } while(0)
+
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) |\
+                           (SEXT(BITS(i, 30, 25), 6) << 5)  |\
+                           (SEXT(BITS(i, 11, 8), 4) << 1)   |\
+                           (SEXT(BITS(i, 7, 7), 1) << 11)   ; } while(0)
+//! end my code
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -43,6 +54,10 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+//! begin my code    
+    case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+//! end my code
   }
 }
 
@@ -61,7 +76,12 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(dest) = imm);
   INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(dest) = Mr(src1 + imm, 4));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
-
+  //! begin my code
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, s->dnpc += imm);
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) += imm);
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", jal    , J, s->dnpc += imm);
+//  INSTPAT("??????? ????? ????? ??? ????? 00101 11", jal    , J, s->dnpc += imm);
+  //! end my code
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
