0490787daac5ea258ec2b56db2d9acbbb8027a32
tracer-ics2022 <tracer@njuics.org>
2022-11-01 12:18:52 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:18:52 up 7 days, 17:33,  1 user,  load average: 0.87, 0.74, 0.61

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,19 +5,25 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-char *int_to_string(int num, char *ans){
+char *int_to_string(int num, char *ans, int zeroflag, int field_width){
   int sign = (num >= 0);
+  int counter = 0;
   if(!sign) num = -num;
   char reverse[1024];
   char *s = reverse;
-  if(num == 0) *s++ = '0';
+  if(num == 0) {*s++ = '0'; counter++;}
   else while(num){
     *s++ = num % 10 + '0';
+    counter++;
     num /= 10;
   }
   *s = '\0';
   size_t len = strlen(reverse);
   if(!sign) *ans++ = '-';
+  if(field_width != -1){
+    for(size_t i = 0; i < field_width - counter; i++)
+      *ans++ = zeroflag? '0' : ' ';
+  }
   for(size_t i = 0; i < len; i++) *ans++ = *(--s);
   return ans;
 }
@@ -27,21 +33,28 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   char *str;
   int num;
   char *s;
+  int zeroflag = false, field_width = -1;
   for(str = out; *fmt; fmt++){
-    if(*fmt != '%'){
-      *str++ = *fmt;
-      continue;
-    }
+    if(*fmt != '%'){ *str++ = *fmt; continue;}
     fmt++;
+    if(*fmt == '0'){ fmt++; zeroflag = true;}
+    if(*fmt >= '0' && *fmt <= '9'){
+      field_width = atoi(fmt);
+      while(*fmt >= '0' && *fmt <= '9') fmt++;
+    }
     switch(*fmt){
       case 'd':
         num = va_arg(ap, int);
-        str = int_to_string(num, str);
+        str = int_to_string(num, str, zeroflag, field_width);
         continue;
       case 's':
       case 'c':
         s = va_arg(ap, char*);
         size_t len = strlen(s);
+        if(field_width != -1){
+          for(size_t i = 0; i < field_width - len; i++)
+            *str++ = zeroflag ? '0' : ' ';
+        }
         for(size_t i = 0; i < len; i++) *str++ = *s++;
         continue; 
       default: panic("Not implemented");
