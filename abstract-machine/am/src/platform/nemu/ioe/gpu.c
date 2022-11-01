3e1da42f1a7bb9b15af60655ae4cb0dbc433634e
tracer-ics2022 <tracer@njuics.org>
2022-11-01 22:42:12 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:42:12 up 8 days,  3:56,  1 user,  load average: 0.35, 0.45, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,21 +3,38 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static uint32_t screen_width() {
+  return (uint32_t)inw(VGACTL_ADDR + 2);
+}
+
+static uint32_t screen_height() {
+  return (uint32_t)inw(VGACTL_ADDR);
+}
+
+static uint32_t screen_size() {
+  return screen_width() * screen_height() * sizeof(uint32_t);
+}
+
 void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = (int)screen_width(), .height = (int)screen_height(),
+    .vmemsz = (int)screen_size()
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t width = screen_width(), height = screen_height();
+  for(int i = 0; i < w; i ++)
+    for(int j = 0; j < h; j ++)
+      if(x + i < width && y + j < height)
+        outl(FB_ADDR + (width * (y + j) + x + i) * 4, pixels[w * j + i]);
+  if(ctl->sync) outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -73,6 +73,9 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  uint32_t reg_sync = vgactl_port_base[1];
+  if(reg_sync) update_screen();
+  vgactl_port_base[1] = 0;
 }
 
 void init_vga() {
