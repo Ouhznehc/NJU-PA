8ba8e22a27c86977aabf14b7d9f926b6b2975ddf
tracer-ics2022 <tracer@njuics.org>
2022-11-20 17:21:12 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:12 up 7 days, 21:18,  1 user,  load average: 1.94, 1.79, 1.66

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -57,6 +57,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  #ifdef CONFIG_MTRACE
+    color_green("INFO: memory read at addr = 0x%08x , len = %02d\n", addr, len);
+  #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
