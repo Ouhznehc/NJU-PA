f722a2f64b43bf955ed294b565ca1288a0db14b1
tracer-ics2022 <tracer@njuics.org>
2022-12-11 23:30:17 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:30:17 up 5 days,  5:08,  1 user,  load average: 0.54, 0.48, 0.54

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,99 +1,3 @@
-// #include <am.h>
-// #include <klib.h>
-// #include <klib-macros.h>
-// #include <stdarg.h>
-
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-// char *int_to_string(int num, char *ans, int zeroflag, int field_width){
-//   int sign = (num >= 0);
-//   int counter = 0;
-//   if(!sign) num = -num;
-//   char reverse[1024];
-//   char *s = reverse;
-//   if(num == 0) {*s++ = '0'; counter++;}
-//   else while(num){
-//     *s++ = num % 10 + '0';
-//     counter++;
-//     num /= 10;
-//   }
-//   *s = '\0';
-//   size_t len = strlen(reverse);
-//   if(!sign) *ans++ = '-';
-//   if(field_width != -1){
-//     for(size_t i = 0; i < field_width - counter; i++)
-//       *ans++ = zeroflag? '0' : ' ';
-//   }
-//   for(size_t i = 0; i < len; i++) *ans++ = *(--s);
-//   return ans;
-// }
-
-
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   char *str;
-//   int num;
-//   char *s;
-//   int zeroflag = false, field_width = -1;
-//   for(str = out; *fmt; fmt++){
-//     if(*fmt != '%'){ *str++ = *fmt; continue;}
-//     fmt++;
-//     if(*fmt == '0'){ fmt++; zeroflag = true;}
-//     if(*fmt >= '0' && *fmt <= '9'){
-//       field_width = atoi(fmt);
-//       while(*fmt >= '0' && *fmt <= '9') fmt++;
-//     }
-//     switch(*fmt){
-//       case 'd':
-//         num = va_arg(ap, int);
-//         str = int_to_string(num, str, zeroflag, field_width);
-//         continue;
-//       case 's':
-//       case 'c':
-//         s = va_arg(ap, char*);
-//         size_t len = strlen(s);
-//         if(field_width != -1){
-//           for(size_t i = 0; i < field_width - len; i++)
-//             *str++ = zeroflag ? '0' : ' ';
-//         }
-//         for(size_t i = 0; i < len; i++) *str++ = *s++;
-//         continue; 
-//       default: break;
-//     }
-//   }
-//   *str = '\0';
-//   return str - out;
-// }
-
-// int sprintf(char *out, const char *fmt, ...) {
-//   va_list args;
-//   int val;
-//   va_start(args, fmt);
-//   val = vsprintf(out, fmt, args);
-//   va_end(args);
-//   return val;
-// }
-
-// int snprintf(char *out, size_t n, const char *fmt, ...) {
-//   panic("Not implemented");
-// }
-
-// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-//   panic("Not implemented");
-// }
-
-// int printf(const char *fmt, ...) {
-//   char out[1024];
-//   int val;
-//   va_list args;
-//   va_start(args, fmt);
-//   val = vsprintf(out, fmt, args);
-//   va_end(args);
-//   putstr(out);
-//   return val;
-// }
-// #endif
-
-
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
@@ -101,136 +5,72 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static char sprint_buf[1024];
-
-int printf(const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int check = vsprintf(sprint_buf, fmt, ap);
-  va_end(ap);
-  for(int i = 0; i < check; i++)
-    putch(sprint_buf[i]);
-  return check;
+char *int_to_string(int num, char *ans, int zeroflag, int field_width){
+  int sign = (num >= 0);
+  int counter = 0;
+  if(!sign) num = -num;
+  char reverse[1024];
+  char *s = reverse;
+  if(num == 0) {*s++ = '0'; counter++;}
+  else while(num){
+    *s++ = num % 10 + '0';
+    counter++;
+    num /= 10;
+  }
+  *s = '\0';
+  size_t len = strlen(reverse);
+  if(!sign) *ans++ = '-';
+  if(field_width != -1){
+    for(size_t i = 0; i < field_width - counter; i++)
+      *ans++ = zeroflag? '0' : ' ';
+  }
+  for(size_t i = 0; i < len; i++) *ans++ = *(--s);
+  return ans;
 }
 
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char *tmp = out;
-  while(*fmt != '\0'){
-    if(*fmt != '%'){
-        *tmp++ = *fmt++;
-        continue;
-    }
-    fmt ++;
-    char *s;
-    int len, i, pos, flag, zeroflag = false, field_width = -1;
-    int64_t num;
-    uint64_t unum;
-    char snum[15];
-    if(*fmt == '0'){
-      fmt ++;
-      zeroflag = true;
-    }
+  char *str;
+  int num;
+  char *s;
+  int zeroflag = false, field_width = -1;
+  for(str = out; *fmt; fmt++){
+    if(*fmt != '%'){ *str++ = *fmt; continue;}
+    fmt++;
+    if(*fmt == '0'){ fmt++; zeroflag = true;}
     if(*fmt >= '0' && *fmt <= '9'){
       field_width = atoi(fmt);
-      while(*fmt >= '0' && *fmt <= '9')
-        fmt ++;
+      while(*fmt >= '0' && *fmt <= '9') fmt++;
     }
-      
-    switch (*fmt)
-    {
-      case 's':
-        s = va_arg(ap, char *);
-        len = strlen(s);
-        if(field_width != -1){
-          for(i = 0; i < field_width - len; i++)
-            *tmp++ = zeroflag?'0':' ';
-        }
-        for(i = 0; i < len; i++)
-          *tmp++ = *s++;
-        break;
+    switch(*fmt){
       case 'd':
         num = va_arg(ap, int);
-        pos = 0;
-        flag = 0;
-        if(num < 0){
-          flag = 1;
-          num *= -1;
-        }
-        if(num == 0){
-          snum[++ pos] = '0';
-        }
-        while(num){
-          snum[++pos] = '0' + (num % 10);
-          num /= 10;
-        }
-        if(flag)
-          *tmp++ = '-';
+        str = int_to_string(num, str, zeroflag, field_width);
+        continue;
+      case 's':
+      case 'c':
+        s = va_arg(ap, char*);
+        size_t len = strlen(s);
         if(field_width != -1){
-          for(i = 0; i < field_width - pos; i++)
-            *tmp++ = zeroflag?'0':' ';
+          for(size_t i = 0; i < field_width - len; i++)
+            *str++ = zeroflag ? '0' : ' ';
         }
-        for(i = 0; i < pos; i++)
-          *tmp++ = snum[pos - i];
-        break;
-      case 'x': 
-        unum = va_arg(ap, uint32_t);
-        pos = 0;
-        if(unum == 0){
-          snum[++ pos] = '0';
-        }
-        while(unum){
-          snum[++pos] = unum % 16;
-          if(snum[pos] < 10)snum[pos] += '0';
-          else snum[pos] += 'a' - 10;
-          unum /= 16;
-        }
-        if(field_width != -1){
-          for(i = 0; i < field_width - pos; i++)
-            *tmp++ = zeroflag?'0':' ';
-        }
-        for(i = 0; i < pos; i++)
-          *tmp++ = snum[pos - i];
-        break;
-      case 'p':
-        unum = (unsigned int)va_arg(ap, void *);
-        pos = 0;
-        if(unum == 0){
-          snum[++ pos] = '0';
-        }
-        while(unum){
-          snum[++pos] = unum % 16;
-          if(snum[pos] < 10)snum[pos] += '0';
-          else snum[pos] += 'a' - 10;
-          unum /= 16;
-        }
-        *tmp++ = '0';
-        *tmp++ = 'x';
-        if(field_width != -1){
-          for(i = 0; i < field_width - pos; i++)
-            *tmp++ = zeroflag?'0':' ';
-        }
-        for(i = 0; i < pos; i++)
-          *tmp++ = snum[pos - i];
-        break;
-      case '%':
-        *tmp++ = '%';
-        break;
-      default:
-        break;
+        for(size_t i = 0; i < len; i++) *str++ = *s++;
+        continue; 
+      default: break;
     }
-
-    fmt ++;
   }
-  *tmp = '\0';
-  return tmp - out;
+  *str = '\0';
+  return str - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int check = vsprintf(out, fmt, ap);
-  va_end(ap);
-  return check;
+  va_list args;
+  int val;
+  va_start(args, fmt);
+  val = vsprintf(out, fmt, args);
+  va_end(args);
+  return val;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -241,4 +81,14 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+int printf(const char *fmt, ...) {
+  char out[1024];
+  int val;
+  va_list args;
+  va_start(args, fmt);
+  val = vsprintf(out, fmt, args);
+  va_end(args);
+  putstr(out);
+  return val;
+}
 #endif
