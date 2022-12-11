0f049d4631efd2483c2f77afa30119e95760c8cd
tracer-ics2022 <tracer@njuics.org>
2022-12-12 02:35:17 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:35:17 up 5 days,  8:13,  1 user,  load average: 0.61, 0.64, 0.62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,9 +11,7 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  ramdisk_read((void *)0x830003fc, 0x0, 0x4db8);
-  //memset()
-  return 0x830003fc;
+  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
