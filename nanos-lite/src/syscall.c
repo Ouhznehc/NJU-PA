df6404b253012f66a96414d725e31d9deb1b6481
tracer-ics2022 <tracer@njuics.org>
2022-12-12 22:47:15 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:47:15 up 6 days,  4:25,  1 user,  load average: 0.60, 0.53, 0.60

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,7 +1,7 @@
 #include <common.h>
 #include "syscall.h"
 #include <proc.h>
-
+#include <fs.h>
 void yield();
 void naive_uload(PCB *pcb, const char *filename);
 void halt(int code);
@@ -11,8 +11,9 @@ void sys_yield(Context *c){ yield(); c->GPRx = 0; }
 
 void sys_exit(Context *c){ halt(0); }
 
-
 void sys_write(Context *c){ c->GPRx = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4); }
+
+void sys_read(Context *c){ c->GPRx = fs_read(c->GPR2, (void *)c->GPR3, c->GPR4); }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -23,6 +24,7 @@ void do_syscall(Context *c) {
     case SYS_yield: sys_yield(c);  break;
     case SYS_exit:  sys_exit(c);   break;
     case SYS_write: sys_write(c);  break;
+    case SYS_read:  sys_read(c);   break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
