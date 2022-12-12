5ec0fc6d150166165fe68f7c4d78cb24f63ab9c5
tracer-ics2022 <tracer@njuics.org>
2022-12-12 16:55:31 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:31 up 5 days, 22:33,  1 user,  load average: 0.44, 0.57, 0.57

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -9,5 +9,14 @@ enum {SEEK_SET, SEEK_CUR, SEEK_END};
 
 #endif
 
+// read/write operation
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+
+// fs operation
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,7 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
+
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -11,10 +13,25 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  // Elf_Ehdr elf_header;
-  // Elf_Phdr program_header;
+  Elf_Ehdr elf_header;
+  int elf_id = fs_open(filename, 0, 0);
+  fs_read(elf_id, &elf_header, sizeof(elf_header));
 
-  return 0;
+  assert(*(uint32_t *)elf_header.e_ident == 0x464C457F);// magic number must be elf
+
+  size_t ph_offest = elf_header.e_phoff;
+
+  for(int i =  0; i < elf_header.e_phnum; i++){
+    Elf_Phdr elf_phdr;
+    fs_lseek(elf_id, ph_offest + i * elf_header.e_phentsize, SEEK_SET);
+    fs_read(elf_id, &elf_phdr, sizeof(elf_phdr));
+    if(elf_phdr.p_type == PT_LOAD){
+      fs_lseek(elf_id, elf_phdr.p_offset, SEEK_SET);
+      fs_read(elf_id, (void *)elf_phdr.p_vaddr, elf_phdr.p_filesz);
+      memset((void *)elf_phdr.p_vaddr + elf_phdr.p_filesz, 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
+    }
+  }
+  return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
