8d4269babe24859c7f3c2bd1750eeb0dbeb6346c
tracer-ics2022 <tracer@njuics.org>
2022-12-12 21:28:53 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:53 up 6 days,  3:06,  1 user,  load average: 0.78, 0.71, 0.67

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,7 +17,7 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   size_t res = len;
   char *now = (char *) buf;
-  while(*now++ && len--) putch(*now);
+  while(*now++ && len--) {printf("--\n");putch(*now);}
   return res;
 }
 
