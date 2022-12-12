7999abeca35eb665e14641783ee62c9529fc3921
tracer-ics2022 <tracer@njuics.org>
2022-12-12 21:48:30 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:48:30 up 6 days,  3:26,  1 user,  load average: 0.69, 0.73, 0.81

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,16 +1,34 @@
+// #include <common.h>
+// void do_syscall(Context *c);
+
+// static Context* do_event(Event e, Context* c) {
+//   switch (e.event) {
+//     case EVENT_YIELD: color_yellow("==== event  yield  triggered ==== \n"); break;
+//     case EVENT_SYSCALL: 
+//       //color_yellow("==== event syscall triggered ==== \n");
+//       do_syscall(c);
+//       break;
+//     default: panic("Unhandled event ID = %d", e.event);
+//   }
+
+//   return c;
+// }
+
+// void init_irq(void) {
+//   Log("Initializing interrupt/exception handler...");
+//   cte_init(do_event);
+// }
+
 #include <common.h>
+
 void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: color_yellow("==== event  yield  triggered ==== \n"); break;
-    case EVENT_SYSCALL: 
-      //color_yellow("==== event syscall triggered ==== \n");
-      do_syscall(c);
-      break;
+    case EVENT_YIELD: printf("YYYYYYYYIELD\n"); break;
+    case EVENT_SYSCALL: do_syscall(c); break; 
     default: panic("Unhandled event ID = %d", e.event);
   }
-
   return c;
 }
 
@@ -18,3 +36,4 @@ void init_irq(void) {
   Log("Initializing interrupt/exception handler...");
   cte_init(do_event);
 }
+
