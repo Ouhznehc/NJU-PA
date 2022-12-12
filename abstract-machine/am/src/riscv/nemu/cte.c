045d6a74063195c46fee96a0419bf9f0e637669d
tracer-ics2022 <tracer@njuics.org>
2022-12-12 21:47:57 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:47:57 up 6 days,  3:25,  1 user,  load average: 0.43, 0.68, 0.80

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -1,67 +1,15 @@
-// #include <am.h>
-// #include <riscv/riscv.h>
-// #include <klib.h>
-
-// static Context* (*user_handler)(Event, Context*) = NULL;
-// #define EVENT 0
-// Context* __am_irq_handle(Context *c) {
-//   if (user_handler) {
-//     Event ev = {0};
-//     switch (c->mcause) {
-//       case EVENT:
-//         if(!~c->GPR1) ev.event = EVENT_YIELD;
-//         else ev.event = EVENT_SYSCALL;
-//         break;
-//       default: ev.event = EVENT_ERROR; break;
-//     }
-
-//     c = user_handler(ev, c);
-//     assert(c != NULL);
-//     c->mepc += 4;
-//   }
-
-//   return c;
-// }
-
-// extern void __am_asm_trap(void);
-
-// bool cte_init(Context*(*handler)(Event, Context*)) {
-//   // initialize exception entry
-//   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-
-//   // register event handler
-//   user_handler = handler;
-
-//   return true;
-// }
-
-// Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-//   return NULL;
-// }
-
-// void yield() {
-//   asm volatile("li a7, -1; ecall");
-// }
-
-// bool ienabled() {
-//   return false;
-// }
-
-// void iset(bool enable) {
-// }
-
 #include <am.h>
 #include <riscv/riscv.h>
 #include <klib.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
-
+#define EVENT 0
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0: 
-        if(c->GPR1 == -1)ev.event = EVENT_YIELD; 
+      case EVENT:
+        if(!~c->GPR1) ev.event = EVENT_YIELD;
         else ev.event = EVENT_SYSCALL;
         break;
       default: ev.event = EVENT_ERROR; break;
@@ -101,4 +49,3 @@ bool ienabled() {
 
 void iset(bool enable) {
 }
-
