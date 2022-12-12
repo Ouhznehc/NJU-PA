afe938c14a5d166d569c4ccc2fe177fd221baad1
tracer-ics2022 <tracer@njuics.org>
2022-12-12 22:35:52 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:35:52 up 6 days,  4:13,  1 user,  load average: 0.51, 0.59, 0.67

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,16 +12,13 @@ void sys_yield(Context *c){
   c->GPRx = 0;
 }
 
-void sys_exit(Context *c){
-  halt(0);
-}
+void sys_exit(Context *c){ halt(c->GPRx); }
 
 void sys_write(Context *c){ c->GPRx = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4); }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
 
   switch (a[0]) {
     case SYS_yield: sys_yield(c); break;
