0b3b4cc75d28d0d8660f224e41808c52c299c600
tracer-ics2022 <tracer@njuics.org>
2022-12-12 22:35:29 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:35:29 up 6 days,  4:13,  1 user,  load average: 0.56, 0.61, 0.68

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,26 +1,33 @@
 #include <common.h>
-#include <fs.h>
 #include "syscall.h"
+#include <proc.h>
 
-void sys_yield(Context *c) { yield(); c->GPRx = 0; }
+void yield();
+void naive_uload(PCB *pcb, const char *filename);
+void halt(int code);
+size_t fs_write(int fd, const void *buf, size_t len);
 
-void sys_exit (Context *c) { halt(c->GPRx); }
+void sys_yield(Context *c){
+  yield();
+  c->GPRx = 0;
+}
 
-void sys_write(Context *c) { c->GPRx = fs_write(c->GPR2, (void*)c->GPR3, c->GPR4); }
+void sys_exit(Context *c){
+  halt(0);
+}
 
-void sys_read (Context *c) { c->GPRx = fs_read(c->GPR2, (void*)c->GPR3, c->GPR4); }
-
-
+void sys_write(Context *c){ c->GPRx = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4); }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
+
   switch (a[0]) {
     case SYS_yield: sys_yield(c); break;
-    case SYS_exit : sys_exit(c);  break;
+    case SYS_exit:  sys_exit(c);  break;
     case SYS_write: sys_write(c); break;
-    case SYS_read : sys_read(c);  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
