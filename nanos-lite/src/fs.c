c1ea4938af49551debf43c571b42b901f6814a71
tracer-ics2022 <tracer@njuics.org>
2022-12-13 01:10:32 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:10:32 up 6 days,  6:48,  1 user,  load average: 0.41, 0.43, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,22 +50,18 @@ int fs_open(const char *pathname, int flags, int mode){
 
 size_t fs_read(int fd, void *buf, size_t len){
   Finfo *file = &file_table[fd];
+  if(file->open_offset + len > file->size && !file->read) len = file->size - file->open_offset;
   if(file->read) file->read(buf, file->disk_offset + file->open_offset, len);
-  else {
-    if(file->open_offset + len > file->size) len = file->size - file->open_offset;
-    ramdisk_read(buf, file->disk_offset + file->open_offset, len);
-  }
+  else ramdisk_read(buf, file->disk_offset + file->open_offset, len);
   file->open_offset += len;
   return len;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len){
   Finfo *file = &file_table[fd];
+  if(file->open_offset + len > file->size && !file->write) len = file->size - file->open_offset;
   if(file->write) file->write(buf, file->disk_offset + file->open_offset, len);
-  else {
-    if(file->open_offset + len > file->size) len = file->size - file->open_offset;
-    ramdisk_write(buf, file->disk_offset + file->open_offset, len);
-  }
+  else ramdisk_write(buf, file->disk_offset + file->open_offset, len);
   file->open_offset += len;
   return len;
 }
