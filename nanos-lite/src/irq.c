5210795ceb56fbea9105fa58ef09cf87e10ccb67
tracer-ics2022 <tracer@njuics.org>
2022-12-14 14:18:20 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:18:20 up 7 days, 19:56,  1 user,  load average: 1.00, 0.64, 0.57

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,9 +3,9 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: color_yellow("=== event  yield  triggered ===\n"); break;
+    case EVENT_YIELD: /*color_yellow("=== event  yield  triggered ===\n");*/ break;
     case EVENT_SYSCALL:
-      color_yellow("=== event syscall triggered ===\n");
+      //color_yellow("=== event syscall triggered ===\n");
       do_syscall(c);
       break;
     default: panic("Unhandled event ID = %d", e.event);
