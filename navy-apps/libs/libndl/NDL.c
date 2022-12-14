eca179e59c4b87c69d75b9f0fc3218fa6aa45ece
tracer-ics2022 <tracer@njuics.org>
2022-12-14 14:35:06 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:06 up 7 days, 20:12,  1 user,  load average: 0.60, 0.54, 0.55

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,13 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-  return 0;
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  uint32_t time_ms = time.tv_sec * 1000 + time.tv_usec / 1000;
+  return time_ms;
 }
 
 int NDL_PollEvent(char *buf, int len) {
