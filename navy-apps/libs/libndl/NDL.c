827631f26abca4d0e6157b17276453ac16c65387
tracer-ics2022 <tracer@njuics.org>
2022-12-14 20:08:17 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:08:17 up 8 days,  1:46,  1 user,  load average: 0.67, 0.57, 0.55

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -27,23 +27,23 @@ int NDL_PollEvent(char *buf, int len) {
 void NDL_OpenCanvas(int *w, int *h) {
   if(*w == 0 && *h == 0){*w = screen_w, *h = screen_h;}
   canvas_w = *w; canvas_h = *h;
-  if (getenv("NWM_APP")) {
-    int fbctl = 4;
-    fbdev = 5;
-    screen_w = *w; screen_h = *h;
-    char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
-    // let NWM resize the window and create the frame buffer
-    write(fbctl, buf, len);
-    while (1) {
-      // 3 = evtdev
-      int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
-      buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
-    }
-    close(fbctl);
-  }
+  // if (getenv("NWM_APP")) {
+  //   int fbctl = 4;
+  //   fbdev = 5;
+  //   screen_w = *w; screen_h = *h;
+  //   char buf[64];
+  //   int len = sprintf(buf, "%d %d", screen_w, screen_h);
+  //   // let NWM resize the window and create the frame buffer
+  //   write(fbctl, buf, len);
+  //   while (1) {
+  //     // 3 = evtdev
+  //     int nread = read(3, buf, sizeof(buf) - 1);
+  //     if (nread <= 0) continue;
+  //     buf[nread] = '\0';
+  //     if (strcmp(buf, "mmap ok") == 0) break;
+  //   }
+  //   close(fbctl);
+  // }
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
@@ -73,7 +73,7 @@ int NDL_QueryAudio() {
 
 int NDL_Init(uint32_t flags) {
   char dispinfo_buf[64];
-  if (getenv("NWM_APP")) evtdev = 3;
+  //if (getenv("NWM_APP")) evtdev = 3;
   int dispinfo = open("/proc/dispinfo", O_RDONLY);
   read(dispinfo, dispinfo_buf, 64);
   sscanf(dispinfo_buf, "WIDTH : %d\nHEIGHT : %d\n", &screen_w, &screen_h);
