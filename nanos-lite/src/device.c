fc9e8b3d7a204c9ca883887ea098f5ee1c9d192a
tracer-ics2022 <tracer@njuics.org>
2022-12-14 18:08:49 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:08:49 up 7 days, 23:46,  1 user,  load average: 0.53, 0.58, 0.57

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,8 +31,10 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-
-  return 0;
+  int width  = io_read(AM_GPU_CONFIG).width;
+  int height = io_read(AM_GPU_CONFIG).height;
+  sprintf(buf, "WIDTH : %d\nHEIGHT : %d\n", width, height);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
