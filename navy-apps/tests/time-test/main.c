be5258957f81975cb2b197daebf4f221b720b886
tracer-ics2022 <tracer@njuics.org>
2022-12-14 14:29:07 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:29:07 up 7 days, 20:06,  1 user,  load average: 0.70, 0.64, 0.59

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -1,15 +1,31 @@
+// #include <stdio.h>
+// #include <stdint.h>
+// #include <assert.h>
+// #include <NDL.h>
+
+// int main() {
+//   uint32_t time = NDL_GetTicks();
+//   while(1){
+//     if(NDL_GetTicks() - time >= 500){
+//       printf("now time is %d\n", time);
+//       time = NDL_GetTicks();
+//     }
+//   }
+//   return 0;
+// }
+
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
 #include <NDL.h>
 
 int main() {
-  uint32_t time = NDL_GetTicks();
+  uint32_t tim = NDL_GetTicks();
   while(1){
-    if(NDL_GetTicks() - time >= 500){
-      printf("now time is %d\n", time);
-      time = NDL_GetTicks();
+    while(NDL_GetTicks() - tim < 500){  
     }
+    printf("time = %d\n", tim);
+    tim = NDL_GetTicks();
   }
   return 0;
 }
