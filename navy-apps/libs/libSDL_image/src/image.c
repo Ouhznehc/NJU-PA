3a97474ab78d581ea49706856ad0321cc90d78a7
tracer-ics2022 <tracer@njuics.org>
2022-12-17 00:47:00 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:47:00 up 1 day,  8:16,  1 user,  load average: 0.52, 0.26, 0.10

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,16 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *file = fopen(filename, "r");
+  fseek(file, 0, SEEK_END);
+  int size = ftell(file);
+  char *buf = malloc(sizeof(char) * size);
+  fseek(file, 0, SEEK_SET);
+  fread(buf, size, 1, file);
+  SDL_Surface *surface = STBIMG_LoadFromMemory(buf, size);
+  fclose(file);
+  free(buf);
+  return surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
