12e1d640693a5e3370c0fd0ee932119a8b40404f
tracer-ics2022 <tracer@njuics.org>
2022-12-16 20:50:50 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:50:50 up 1 day,  4:20,  1 user,  load average: 0.48, 0.32, 0.23

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,14 +14,27 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  char key_buf[64], key_name[64];
+  int key_index = 0;
+  if(!NDL_PollEvent(key_buf, sizeof(key_buf))) return 0; // return intsantly
+  if(key_buf[4] == 'd')ev->type = SDL_KEYDOWN; // key down xxx
+  else ev->type = SDL_KEYUP; // key up   xxx
+  for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
+    key_name[key_index] = key_buf[key_index + key_offset];
+  key_name[key_index] = '\0';
+
+  for(int i = 0; i < sizeof(keyname) / sizeof(char *); i++)
+    if (strcmp(key_name, keyname[i]) == 0){
+      ev->key.keysym.sym = i;
+    }
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
   char key_buf[64], key_name[64];
   int ret = 0, key_index = 0;
   while(!NDL_PollEvent(key_buf, sizeof(key_buf))); // wait event
-  if(key_buf[4] == 'd')event->type = SDL_KEYDOWN; // key down xxx
+  if(key_buf[4] == 'd') event->type = SDL_KEYDOWN; // key down xxx
   else event->type = SDL_KEYUP; // key up   xxx
   for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
     key_name[key_index] = key_buf[key_index + key_offset];
