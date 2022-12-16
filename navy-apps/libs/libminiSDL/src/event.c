65e1f7420704d7ef88bc597a4da297fd89438705
tracer-ics2022 <tracer@njuics.org>
2022-12-16 16:08:35 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:08:35 up 23:38,  1 user,  load average: 0.89, 0.44, 0.32

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,8 +1,9 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
-
+#define key_offset 9
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
@@ -17,8 +18,21 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  
-  return 1;
+  char key_buf[64], key_name[64];
+  int ret = 0, key_index = 0;
+  while(!NDL_PollEvent(key_buf, sizeof(key_buf))); // wait event
+  if(key_buf[4] == 'd')event->type = SDL_KEYDOWN; // key down xxx
+  else event->type = SDL_KEYUP; // key up   xxx
+  for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
+    key_name[key_index] = key_buf[key_index + key_offset];
+  key_name[key_index] = '\0';
+
+  for(int i = 0; i < sizeof(keyname) / sizeof(char *); i++)
+    if (strcmp(key_name, keyname[i]) == 0){
+      event->key.keysym.sym = i;
+      ret = 1;
+    }
+  return ret;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
