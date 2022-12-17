62a8637cd2248b7a2e1b3d5d40e19ddcf2b67777
tracer-ics2022 <tracer@njuics.org>
2022-12-17 14:07:05 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:07:05 up 1 day, 21:36,  1 user,  load average: 0.17, 0.13, 0.28

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <assert.h>
 
+enum{PollEvent, WaitEvent};
+
 #define keyname(k) #k,
 #define key_offset 9
 static const char *keyname[] = {
@@ -15,29 +17,14 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  char key_buf[64], key_name[64];
+int SDL_DealEvent(SDL_Event *event, int select) {
+ char key_buf[64], key_name[64];
   int key_index = 0;
-  if(!NDL_PollEvent(key_buf, sizeof(key_buf))) return 0; // return intsantly
-  if(key_buf[4] == 'd')ev->type = SDL_KEYDOWN; // key down xxx
-  else ev->type = SDL_KEYUP; // key up   xxx
-  for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
-    key_name[key_index] = key_buf[key_index + key_offset];
-  key_name[key_index] = '\0';
-
-  for(int i = 0; i < sizeof(keyname) / sizeof(char *); i++)
-    if (strcmp(key_name, keyname[i]) == 0){
-      ev->key.keysym.sym = i;
-      key_state[i] = ~ev->type;
-    }
-  return 1;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
-  char key_buf[64], key_name[64];
-  int key_index = 0;
-  while(!NDL_PollEvent(key_buf, sizeof(key_buf))); // wait event
-  if(key_buf[4] == 'u') event->type = SDL_KEYUP; // key up   xxx
+  if(select == PollEvent)
+    if(!NDL_PollEvent(key_buf, sizeof(key_buf))) return 0; // return intsantly
+  if(select == WaitEvent)
+    while(!NDL_PollEvent(key_buf, sizeof(key_buf)));
+  if(key_buf[4] == 'u')event->type = SDL_KEYUP; // key up   xxx
   else event->type = SDL_KEYDOWN; // key down xxx
   for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
     key_name[key_index] = key_buf[key_index + key_offset];
@@ -49,6 +36,14 @@ int SDL_WaitEvent(SDL_Event *event) {
       key_state[i] = ~event->type;
     }
   return 1;
+}
+
+int SDL_PollEvent(SDL_Event *ev) {
+  return SDL_DealEvent(ev, PollEvent);
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  return SDL_DealEvent(event, WaitEvent);
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
