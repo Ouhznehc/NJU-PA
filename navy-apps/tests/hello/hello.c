f69feed7b5e476fc08f78285b6ed3c5eeda97341
tracer-ics2022 <tracer@njuics.org>
2022-12-18 23:25:54 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:25:54 up 3 days,  6:55,  1 user,  load average: 0.52, 0.18, 0.05

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,14 +4,10 @@
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
-  volatile int j = 0;
+  volatile int j = 100;
   printf("yzhnmb\n");
-  // while (1) {
-  //   j ++;
-  //   if (j == 10000) {
-  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  //     j = 0;
-  //   }
-  // }
+  while (j--) {
+      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+  }
   return 0;
 }
