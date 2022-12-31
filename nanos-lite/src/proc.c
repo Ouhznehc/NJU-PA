b643facbeb17635e15d9c292336cbdddca892b67
tracer-ics2022 <tracer@njuics.org>
2022-12-31 16:28:44 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:28:44 up 6 days, 22:43,  1 user,  load average: 0.14, 0.08, 0.02

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -16,7 +16,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (uintptr_t)arg, j);
     j ++;
     yield();
   }
@@ -29,8 +29,8 @@ void init_proc() {
 
   // naive_uload(NULL, "/bin/hello");
 
-  context_kload(&pcb[0], hello_fun, (void *)1);
-  context_kload(&pcb[1], hello_fun, (void *)(-1));
+  context_kload(&pcb[0], hello_fun, "zero");
+  context_kload(&pcb[1], hello_fun, "one ");
   switch_boot_pcb();
 }
 
