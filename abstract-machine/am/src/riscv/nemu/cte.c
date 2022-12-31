f7fd798c840ef3a60ce1047924509c92cea99282
tracer-ics2022 <tracer@njuics.org>
2022-12-31 15:53:37 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:37 up 6 days, 22:08,  1 user,  load average: 0.01, 0.03, 0.06

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -37,7 +37,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *context = kstack.end - sizeof(Context);
-
+  context->mstatus = 0x1800;
   return context;
 }
 
