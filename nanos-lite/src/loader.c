c05fcf97425835a95028877172cc597b549e9991
tracer-ics2022 <tracer@njuics.org>
2023-01-01 11:54:42 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:54:42 up 7 days, 18:09,  1 user,  load average: 0.00, 0.08, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,13 +45,12 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
 
 }
 
-void context_uload(PCB *pcb, const char *filename) {
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   void *entry = (void *)loader(pcb, filename);
   Area kstack;
   kstack.start = &pcb->cp;
   kstack.end = &pcb->cp + STACK_SIZE;
   Context *context = ucontext(&pcb->as, kstack, entry);
-  Log("--------");
   pcb->cp = context;
   context->GPRx = (uintptr_t)heap.end;
 }
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -8,7 +8,7 @@ PCB *current = NULL;
 
 extern void naive_uload(PCB *pcb, const char *filename);
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
-void context_uload(PCB *pcb, const char *filename);
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -31,7 +31,7 @@ void init_proc() {
   // naive_uload(NULL, "/bin/hello");
 
   context_kload(&pcb[0], hello_fun, "zero");
-  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[1], "/bin/pal", NULL, NULL);
   switch_boot_pcb();
 }
 
