24348439785fd43af66aeeb53f61d92de342b8b3
tracer-ics2022 <tracer@njuics.org>
2023-01-01 21:09:32 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:09:32 up 8 days,  3:23,  1 user,  load average: 0.00, 0.07, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -98,6 +98,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   int envc = 0, argc = 0;
   while(envp && envp[envc++])printf("envc = %d, envp[envc] = %s\n", envc, envp[envc]);
   while(argv && argv[argc++])printf("argc = %d, argv[argc] = %s\n", argc, argv[argc]);
+  printf("%d %d\n", argc, envc);
   char *argv_area[argc], *envp_area[envc];
   char *string_area = (char *)heap.end;
 
