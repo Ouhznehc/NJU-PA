044dcf1835367a6f5dea29cf09e3e67e29ea5ce0
tracer-ics2022 <tracer@njuics.org>
2023-01-02 20:53:21 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:53:21 up 9 days,  3:07,  1 user,  load average: 0.47, 0.42, 0.29

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -18,13 +18,13 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_DealEvent(SDL_Event *event, int select) {
-  return 1;
  char key_buf[64], key_name[64];
   int key_index = 0;
   if(select == PollEvent)
     if(!NDL_PollEvent(key_buf, sizeof(key_buf))) return 0; // return intsantly
   if(select == WaitEvent)
     while(!NDL_PollEvent(key_buf, sizeof(key_buf))); // wait event
+  return 1;
   if(key_buf[4] == 'u')event->type = SDL_KEYUP; // key up   xxx
   else event->type = SDL_KEYDOWN; // key down xxx
   for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
