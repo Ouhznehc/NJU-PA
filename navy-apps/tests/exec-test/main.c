da392014b6cdcdd8d492436e5f45acdd23cad693
tracer-ics2022 <tracer@njuics.org>
2023-01-02 16:29:54 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:29:54 up 8 days, 22:44,  1 user,  load average: 0.22, 0.24, 0.17

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   printf("exec-test begin!!!\n");
   int n = (argc >= 2 ? atoi(argv[1]) : 1);
   printf("%s: argv[1] = %d\n", argv[0], n);
-
+  printf("-------\n");
   char buf[16];
   sprintf(buf, "%d", n + 1);
   execl(argv[0], argv[0], buf, NULL);
