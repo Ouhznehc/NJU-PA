dbe0f56a4886f46ba9f1869019ac5a91b35e2eef
tracer-ics2022 <tracer@njuics.org>
2023-01-02 20:54:09 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:54:09 up 9 days,  3:08,  1 user,  load average: 0.86, 0.53, 0.33

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -24,13 +24,12 @@ int SDL_DealEvent(SDL_Event *event, int select) {
     if(!NDL_PollEvent(key_buf, sizeof(key_buf))) return 0; // return intsantly
   if(select == WaitEvent)
     while(!NDL_PollEvent(key_buf, sizeof(key_buf))); // wait event
-  return 1;
   if(key_buf[4] == 'u')event->type = SDL_KEYUP; // key up   xxx
   else event->type = SDL_KEYDOWN; // key down xxx
   for(key_index = 0; key_buf[key_index + key_offset] != '\n'; key_index++)
     key_name[key_index] = key_buf[key_index + key_offset];
   key_name[key_index] = '\0';
-
+  return 1;
   for(int i = 0; i < sizeof(keyname) / sizeof(char *); i++)
     if (strcmp(key_name, keyname[i]) == 0){
       event->key.keysym.sym = i;
