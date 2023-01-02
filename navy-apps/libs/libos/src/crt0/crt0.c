570f8f50eac71f8bea126a94f1075b6e033cda7e
tracer-ics2022 <tracer@njuics.org>
2023-01-02 17:40:28 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:40:28 up 8 days, 23:54,  1 user,  load average: 0.75, 0.33, 0.23

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,10 +6,15 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  int argc = *args;
-  char **argv = (char **)(args + 1);
-  while(*args++);
-  char **envp = (char **)(args + 1);
+  // int argc = *args;
+  // char **argv = (char **)(args + 1);
+  // while(*args++);
+  // char **envp = (char **)(args + 1);
+   uint32_t* argcpt=(uint32_t*)args;
+  uint32_t st=(uint32_t)args;
+  int argc = argcpt[0];
+char** argv = (char**)(st+4);
+  char** envp = (char**)(st+(argc+2)*4);
   environ = envp;
   exit(main(argc, argv, envp));
   assert(0);
