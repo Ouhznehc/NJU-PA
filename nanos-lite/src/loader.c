05e454f8dbddd0012071bccf1f7b33f061251cb3
tracer-ics2022 <tracer@njuics.org>
2023-01-02 17:15:35 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:15:35 up 8 days, 23:30,  1 user,  load average: 0.09, 0.22, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -98,12 +98,13 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 // to allocate space as the graph above
   int envc = 0, argc = 0;
   while(argv && argv[argc]) {printf("Argument argv[%d] = %s\n", argc, argv[argc]); argc++;}
+  if(envp) {printf("check\n"); printf("%s\n", envp[0]);}
   while(envp && envp[envc]) {printf("Argument envp[%d] = %s\n", envc, envp[envc]); envc++;}
 
   char *argv_area[argc], *envp_area[envc];
   char *string_area = (char *)new_page(nr_page);
   //char *string_area = (char *)heap.end;
-  
+
   for (int i = 0; i < argc; i++){
     string_area -= rounded4(strlen(argv[i]) + 1);
     argv_area[i] = string_area;
