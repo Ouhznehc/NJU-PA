6849ae97bc21f6d5674ba73275b8799f73304821
tracer-ics2022 <tracer@njuics.org>
2023-01-02 17:05:38 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:05:38 up 8 days, 23:20,  1 user,  load average: 0.16, 0.14, 0.16

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -9,6 +9,6 @@ int main(int argc, char *argv[]) {
   printf("%s: argv[1] = %d\n", argv[0], n);
   char buf[16];
   sprintf(buf, "%d", n + 1);
-  execl(argv[0], argv[0], buf, NULL);
+  execl(argv[0], argv[0], buf, 0);
   return 0;
 }
