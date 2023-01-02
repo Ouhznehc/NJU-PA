0319c900bd231b59137cacb2f005402ec16837ac
tracer-ics2022 <tracer@njuics.org>
2023-01-02 18:06:00 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:06:00 up 9 days, 20 min,  1 user,  load average: 0.08, 0.10, 0.14

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ void init_proc() {
   //char *argv[] = {"--skip", "--splash", NULL};
   //context_uload(&pcb[1], "/bin/pal", argv, NULL);
   char *argv[] = {"/bin/exec-test", "0", NULL};
-  context_uload(&pcb[1], "/bin/nslider", argv, NULL);
+  context_uload(&pcb[1], "/bin/nterm", argv, NULL);
   switch_boot_pcb();
 }
 
