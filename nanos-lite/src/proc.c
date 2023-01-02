89a18ac0f1317bebf3a331deb038598b65a2fd89
tracer-ics2022 <tracer@njuics.org>
2023-01-02 18:05:47 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:05:47 up 9 days, 20 min,  1 user,  load average: 0.01, 0.09, 0.14

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ void init_proc() {
   //char *argv[] = {"--skip", "--splash", NULL};
   //context_uload(&pcb[1], "/bin/pal", argv, NULL);
   char *argv[] = {"/bin/exec-test", "0", NULL};
-  context_uload(&pcb[1], "/bin/exec-test", argv, NULL);
+  context_uload(&pcb[1], "/bin/nslider", argv, NULL);
   switch_boot_pcb();
 }
 
@@ -47,12 +47,6 @@ Context* schedule(Context *prev) {
 }
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
-  if(envp){
-    for(int i = 0; envp[i]; i++){
-      printf("----\n");
-      printf("%s\n", envp[i]);
-    }
-  }
   context_uload(pcb_select, filename, argv, envp);
   switch_boot_pcb();
   yield();
