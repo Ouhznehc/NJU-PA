c10bd61dc03af99e566e47cdb4d1a92ccc651d4a
tracer-ics2022 <tracer@njuics.org>
2023-01-05 19:54:42 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:54:42 up 12 days,  2:09,  1 user,  load average: 0.11, 0.13, 0.09

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -31,6 +31,6 @@ typedef struct {
   } inst;
 } riscv32_ISADecodeInfo;
 
-#define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
+#define isa_mmu_check(vaddr, len, type) (cpu.satp & (1ul << 31) ? MMU_TRANSLATE : MMU_DIRECT)
 
 #endif
--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -17,6 +17,26 @@
 #include <memory/paddr.h>
 #include <memory/vaddr.h>
 
+#define PTE_V 0x01
+#define MYBITMASK(bits) ((1u << (bits)) - 1)
+#define MYBITS(x, hi, lo) (((vaddr_t)(x) >> (lo)) & MYBITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
+#define VPN_0(x)  MYBITS(x, 21, 12)
+#define VPN_1(x)  MYBITS(x, 31, 22)
+#define OFFSET(x) MYBITS(x, 11, 0)
+#define PPN(x)    MYBITS(x, 31, 10)
+#define PPN_MASK  (0xfffffc00u)
+typedef uintptr_t PTE;
+
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  return MEM_RET_FAIL;
+  paddr_t pte_addr = (cpu.satp << 12) + VPN_1(vaddr) * 4;
+  PTE pte = paddr_read(pte_addr, 4);
+  assert(pte & PTE_V);
+
+  paddr_t leaf_pte_addr = PPN(pte) * 4096 + VPN_0(vaddr) * 4;
+  PTE leaf_pte = paddr_read(leaf_pte_addr, 4);
+  assert((leaf_pte & PTE_V) == 1);
+
+  paddr_t pa = PPN(leaf_pte) * 4096 + OFFSET(vaddr);
+  assert(pa == vaddr);
+  return pa;
 }
