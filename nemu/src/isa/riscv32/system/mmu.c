1f02a1539a1623b4a6c4d8e202d902c0d770ec13
tracer-ics2022 <tracer@njuics.org>
2023-01-08 11:14:10 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:14:10 up 14 days, 17:28,  1 user,  load average: 0.05, 0.07, 0.08

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -18,6 +18,12 @@
 #include <memory/vaddr.h>
 
 #define PTE_V 0x01
+#define PTE_R 0x02
+#define PTE_W 0x04
+#define PTE_X 0x08
+#define PTE_U 0x10
+#define PTE_A 0x40
+#define PTE_D 0x80
 #define MYBITMASK(bits) ((1u << (bits)) - 1)
 #define MYBITS(x, hi, lo) (((vaddr_t)(x) >> (lo)) & MYBITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
 #define VPN_0(x)  MYBITS(x, 21, 12)
@@ -26,15 +32,19 @@
 #define PPN(x)    MYBITS(x, 31, 10)
 #define PPN_MASK  (0xfffffc00u)
 typedef uintptr_t PTE;
+#define pte_addr (cpu.satp << 12) + VPN_1(vaddr) * 4
+#define leaf_pte_addr PPN(pte) * 4096 + VPN_0(vaddr) * 4
+#define WRITE 1
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  paddr_t pte_addr = (cpu.satp << 12) + VPN_1(vaddr) * 4;
   PTE pte = paddr_read(pte_addr, 4);
   assert(pte & PTE_V);
 
-  paddr_t leaf_pte_addr = PPN(pte) * 4096 + VPN_0(vaddr) * 4;
   PTE leaf_pte = paddr_read(leaf_pte_addr, 4);
   assert(leaf_pte & PTE_V);
+
+  if(type == WRITE) paddr_write(leaf_pte_addr, 4, leaf_pte | PTE_D);
+  else paddr_write(leaf_pte_addr, 4, leaf_pte | PTE_A);
 
   paddr_t pa = PPN(leaf_pte) * 4096 + OFFSET(vaddr);
   assert(pa == vaddr);
