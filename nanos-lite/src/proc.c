493992d938fd3ae5b54fa5df45a5ff64fceb8a49
tracer-ics2022 <tracer@njuics.org>
2023-01-09 20:03:23 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:03:23 up 16 days,  2:17,  1 user,  load average: 0.16, 0.10, 0.06

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -49,7 +49,7 @@ Context* schedule(Context *prev) {
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
   if(fs_open(filename, 0, 0) == -2) return -2;
-  context_uload(&pcb[1], filename, argv, envp);
+  context_uload(current, filename, argv, envp);
   switch_boot_pcb();
   yield();
   return 0;
