2bd4bc7dc59ed0c29e23a146dda61b82202411f1
tracer-ics2022 <tracer@njuics.org>
2023-01-11 15:23:28 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:23:28 up 17 days, 21:37,  1 user,  load average: 0.26, 0.20, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,6 +29,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       int nr_page = ROUNDUP(phdr[i].p_memsz, PGSIZE) / PGSIZE;
       void *page = new_page(nr_page);
       memset(page, 0, nr_page * PGSIZE);
+      Log("this is loader");
       for(int i = 0; i < nr_page; i++) map(&pcb->as, (void *)phdr[i].p_vaddr + i * PGSIZE, page + i * PGSIZE, MMAP_READ | MMAP_WRITE);
       fs_read (fd, page, phdr[i].p_memsz);
     }
@@ -56,6 +57,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   AddrSpace *as = &pcb->as;
   protect(as);
   void *page = new_page(NR_PAGE) + NR_PAGE * PGSIZE;
+  Log("this is uload");
   for(int i = NR_PAGE; i >= 1; i--) map(as, as->area.end - i * PGSIZE, page - i * PGSIZE, MMAP_READ | MMAP_WRITE);
 /*
               |               |
