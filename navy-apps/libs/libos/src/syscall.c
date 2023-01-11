00f0f97332c517f1206c0cb59bbc8cf0e4fa3eae
tracer-ics2022 <tracer@njuics.org>
2023-01-11 17:27:37 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:37 up 17 days, 23:42,  1 user,  load average: 0.30, 0.29, 0.22

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -67,42 +67,16 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
-// extern char end;
-// void *program_break = &end;
-// void *_sbrk(intptr_t increment) {
-//   //printf("_sbrk: increment: %08x   brk: %08p\n", increment, program_break);
-//   //increment = 0;
-//   void *former_program_break = program_break;
-//   int ret = _syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
-//   if(ret == 0) program_break += increment;
-//   else assert(0);
-//   return (void*)former_program_break;
-// }
-
-// extern char end;
-// static void *heap_end = &end;
-// void* _sbrk(intptr_t increment) {
-//   void *last_end = heap_end;
-//   _syscall_(SYS_brk, (intptr_t) (heap_end += increment), 0, 0);
-//   return last_end;
-// }
-
 extern char end;
-void * program_break = NULL;
+void *program_break = &end;
 void *_sbrk(intptr_t increment) {
-  if (program_break == NULL){// 初始化
-    program_break = &end;
-  }
-  void *old_program_break = program_break;
-  
+  //printf("_sbrk: increment: %08x   brk: %08p\n", increment, program_break);
+  //increment = 0;
+  void *former_program_break = program_break;
   int ret = _syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
-  if (ret == 0){
-    program_break = program_break + increment;
-  }else {
-    assert(0);
-  }
-  
-  return old_program_break;
+  if(ret == 0) program_break += increment;
+  else assert(0);
+  return (void*)former_program_break;
 }
 
 int _read(int fd, void *buf, size_t count) {
