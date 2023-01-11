3f1d0d7ee4f0308dac071b43890420937cee621d
tracer-ics2022 <tracer@njuics.org>
2023-01-11 11:28:29 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:28:29 up 17 days, 17:42,  1 user,  load average: 0.31, 0.25, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,18 +14,18 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
-  int elf_id = fs_open(filename, 0, 0);
-  if(elf_id == -1) panic("Invalid filename!");
-  fs_read(elf_id, &ehdr, sizeof(Elf_Ehdr));
+  int fd = fs_open(filename, 0, 0);
+  if(fd == -1) panic("Invalid filename!");
+  fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
 
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);// magic number must be elf
 
   Elf_Phdr phdr[ehdr.e_phnum];
-  fs_read(elf_id, phdr, ehdr.e_phnum * sizeof(Elf_Phdr));
+  fs_read(fd, phdr, ehdr.e_phnum * sizeof(Elf_Phdr));
   for(size_t i = 0; i < ehdr.e_phnum; i++){
     if(phdr[i].p_type == PT_LOAD){
-      fs_lseek(elf_id, phdr[i].p_offset, SEEK_SET);
-      fs_read(elf_id, (void *)phdr[i].p_vaddr, phdr[i].p_memsz);
+      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+      fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_memsz);
       memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
