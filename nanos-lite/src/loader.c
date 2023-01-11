fdcd407121a77db9d7d06d8c0da785d3758ef755
tracer-ics2022 <tracer@njuics.org>
2023-01-11 20:43:35 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:43:35 up 18 days,  2:58,  1 user,  load average: 0.34, 0.20, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,7 +26,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   for(size_t i = 0; i < ehdr.e_phnum; i++){
     if(phdr[i].p_type != PT_LOAD) continue;
     fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
-    int nr_page = ROUNDUP(phdr[i].p_memsz, PGSIZE) / PGSIZE;
+    int nr_page = (phdr[i].p_vaddr + phdr[i].p_memsz) / PGSIZE - phdr[i].p_vaddr / PGSIZE + 1;
     void *page = new_page(nr_page);
     void *vaddr = (void *)ROUNDDOWN(phdr[i].p_vaddr, PGSIZE);
     memset(page, 0, nr_page * PGSIZE);
