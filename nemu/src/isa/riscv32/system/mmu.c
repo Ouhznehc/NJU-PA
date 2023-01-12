03c4d46ca1941d11be331ff371ef5b19cd587918
tracer-ics2022 <tracer@njuics.org>
2023-01-12 13:53:55 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:53:55 up 18 days, 20:08,  1 user,  load average: 0.02, 0.07, 0.08

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -38,7 +38,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   if((pde & PTE_V) == 0) panic("page directory entry: 0x%08x is invalid, vaddr is 0x%08x", pde, vaddr);
 
   uint32_t pte = paddr_read(pte_addr, 4);
-  if((pte & PTE_V) == 0) panic("page table     entry: 0x%08x is invalid, vaddr is 0x%08x", pte, vaddr); 
+  if((pte & PTE_V) == 0) panic("page table entry: 0x%08x is invalid, vaddr is 0x%08x", pte, vaddr); 
 
   uint32_t paddr = PPN(pte) * 4096 + OFFSET(vaddr);
   return paddr;
