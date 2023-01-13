82775738f56d89ee163bce945808a3b2c87aeff2
tracer-ics2022 <tracer@njuics.org>
2023-01-13 10:36:26 +0800

    >  compile NEMU 211240094 陈卓 Linux chenzhuo-KLVC-WXX9 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:36:26 up 19 days, 16:50,  1 user,  load average: 0.04, 0.16, 0.15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -149,8 +149,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   kstack.end = &pcb->cp + STACK_SIZE;
   Context *context = ucontext(as, kstack, entry);
   pcb->cp = context;
-  //context->gpr[2]  = (uintptr_t)ptr - (uintptr_t)page + (uintptr_t)as->area.end;
-  context->GPRx = (uintptr_t)ptr - (uintptr_t)page + (uintptr_t)as->area.end + 4;
-  //context->GPRx = (intptr_t)ptr;
+  context->GPRx = (uintptr_t)ptr - (uintptr_t)page + (uintptr_t)as->area.end;
 }
 
